f00e8ee0d133deaedce34f31a429f0402e3e7c41
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-10-21 21:33:29 -0500

    Variables y tipos de datos 21 10 2021

---
+++ main.js
@@ -0,0 +1,60 @@
+// Que es una variableURL
+// https://desarrolloweb.com/articulos/508.php
+
+// Tipos de datosURL
+// https://www.netinetidesign.com/post/tipos-de-datos-en-javascript/
+
+// Datos String
+let nombre = 'Miguel Angel Castro Escamilla';
+console.log(nombre);
+console.log(Object(nombre));
+
+// Dato Number
+let edad = 23;
+console.log(edad);
+console.log(Object(edad));
+
+// Dato Number
+let altura = 1.63;
+console.log(altura);
+console.log(Object(altura));
+
+// Dato boolean
+let soyProfesor = true;
+console.log(soyProfesor);
+console.log(Object(soyProfesor));
+
+// Datos null
+let pelo = null;
+console.log(pelo);
+console.log(Object(pelo));
+
+// Dato undefined
+let brazos; 
+console.log(brazos);
+console.log(Object(brazos));
+
+// Datos Array
+let pasatiempos = ["Cocinar", "Programar", "leer"];
+console.log(pasatiempos);
+
+// Datos Array
+let contacto = [];
+contacto['Celular'] = 3043640755;
+contacto['email'] = "educacioncontinua04@unc.edu.co";
+console.log(contacto);
+
+// Datos Objeto
+let dirrecion = {
+    calle: "11 b",
+    numero: "85-94",
+    departamento: "Santander"
+};
+console.log(dirrecion);
+
+
+
+
+
+
+
